4cb5206b6351ae65220819b8e4edd0465f4b6239
SrKomodo <komodosr@gmail.com>
2017-09-05 15:25:26 -0300

    do something idk what because i dont remember

--- SFML_Pacman/SFML_Pacman/Main.cpp
+++ SFML_Pacman/SFML_Pacman/Main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <random>
 #include <SFML\Graphics.hpp>
-
-#include "Map.h"
-#include "Player.h"
-#include "Monster.h"
 
 int main() {
 
@@ -13,12 +8,6 @@ int main() {
 	sf::Time ups = sf::seconds(1.f / 60.f);
 
 	sf::RenderWindow window(sf::VideoMode(448,496), "Pacman");
-
-	Map map("Recursos/map.txt");
-
-	Player player = Player(&map, "Recursos/pacman.png", sf::Vector2f(224, 376));
-
-	Monster monster = Monster(&map, "Recursos/ghost.png", sf::Vector2f(232, 184));
 
 	sf::Event event;
 
@@ -31,26 +20,16 @@ int main() {
 				window.close();
 				break;
 
-			case sf::Event::KeyPressed:
-				switch (event.key.code) {
-
-				}
-				break;
 			}
 		}
 
 		while (accumulator > ups) {
-			accumulator -= ups;
-			player.updateLogic(monster.getPos());
-			monster.updateLogic();
+			// update logic	
 		}
 
 		window.clear();
 		
-		map.draw(&window);
-		player.draw(&window);
-
-		monster.draw(&window);
+		// draw
 
 		window.display();
 		accumulator += clock.restart();
